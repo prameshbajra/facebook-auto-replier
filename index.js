fcbfa8b564424548a989290702a116d8dea12f4f
prameshbajra <pe.messh@gmail.com>
2019-12-29 13:25:24 +0530

    Auto replier

--- index.js
+++ index.js
@@ -2,19 +2,31 @@ require('dotenv').config();
 
 const fs = require("fs");
 
-const { Client } = require('libfb')
+const {
+    Client
+} = require('libfb')
 const client = new Client()
 
 client.login(process.env.FB_EMAIL, process.env.FB_PASSWORD).then(() => {
-    const messages = [`Hey, what's up?`, `Please ignore this message`];
-    const messageUsers = JSON.parse(process.env.MESSAGE_USERS);
+    // Record messages and the user details ...
+    client.on(`message`, async message => {
+        const userId = message.threadId;
+        const incomingMessage = message.message;
 
-    for (const [userName, userId] of Object.entries(messageUsers)) {
-        const pickedMessage = messages[Math.floor(Math.random() * messages.length)];
-        console.log(`Sending message ${pickedMessage} to ${userName}`);
-        client.sendMessage(userId + ``, pickedMessage);
-        console.log(`DONE !!`);
-    }
+        const userInfo = await client.getUserInfo(userId);
+        const content = {
+            [userId]: userInfo
+        };
+
+        // fs.appendFile(`./logs/log-2019-12-29.txt`, JSON.stringify(content), function (err) {
+        //     if (err) throw err;
+        //     console.log(`Saved !`);
+        // });
+        console.log(JSON.stringify(content));
+
+        client.sendMessage(userId,
+            `Hey ${userInfo.name}, I am currently working on something. Will get back to you later. Don't mind :)`);
+    });
 }).catch((error) => {
     console.error(`Error here :-> ${error}`);
 });
